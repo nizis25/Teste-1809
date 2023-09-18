da4c31c9c4bbc1112edeb8f064396777cc7d5760
Samuel <samuelpcamargo2005@gmail.com>
2023-09-18 16:53:28 -0300

    c sharp

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,10 @@ namespace Teste_1809
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Cadastro");
+        }
     }
 }
